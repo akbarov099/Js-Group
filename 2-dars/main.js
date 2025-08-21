b1655535b074cababa05fc443f78173a798b80de
akbarov099 <akbarov099@icloud.com>
2025-08-21 09:20:59 +0600

    2-dars

---
+++ 2-dars/main.js
@@ -0,0 +1,48 @@
+// let name = 'akbar'
+// var lastname = 'akbarov'
+// const age = 18
+
+// console.log(`Mening ismim ${name}, familyam ${lastname} va mening yoshim ${age}`);
+
+// alert(name)
+// const age = prompt('Sizning yoshingiz nechida?')
+// console.log(age);
+
+// confirm('Siz bu tovar real ochirmoqchimisz ?')
+
+// const age = prompt('Sizning yoshiingiz nechida?', 100)
+
+// alert(age)
+
+
+// let asdfgh = confirm("Ты здесь главный?");
+
+// console.log(asdfgh);
+
+
+const tekst = 'Hello world'
+console.log(typeof(tekst));
+
+const number = 12345
+
+console.log(typeof(number));
+
+const bigint = 12345678909876543212345678987n
+
+console.log(typeof(bigint));
+
+const boolen = false
+
+console.log(typeof(boolen));
+
+const symbol = Symbol('id')
+
+console.log(typeof(symbol));
+
+const ashibka = undefined
+
+console.log(typeof(ashibka));
+
+const pustoy = null
+
+console.log(typeof(pustoy));
