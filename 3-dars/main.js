76dbaf51570f0701f19e278ea1a2e1a566e19b3b
akbarov099 <akbarov099@icloud.com>
2025-08-26 09:40:57 +0600

    ok

---
+++ 3-dars/main.js
@@ -0,0 +1,38 @@
+// let a = '5';
+// let b = '5';
+
+// console.log(+a + +b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a ** b);
+// console.log(a % b);
+
+// let a = 5;
+
+// let b = 3;
+
+// if (a & b > 9) {
+//   alert("Salom");
+// } else alert("Hayir");
+
+let age = prompt("Yoshingizni kiriting"); 7
+
+if (age >= 18) {
+  alert("Siz balogat yoshigan yetkansiz");
+} else if (age < 5) {
+  alert("siz hali godaksiz");
+} else {
+  alert("Sizga mumkun emas");
+}
+
+
+// = odiy raven
+// == proverka
+// === strogiy raven
+// != ne raven
+// !== ne strogiy raven
+// > bolshe menshe
+// < bolshe menshe
+// & ii
+// || iili
