3c6d5a3cfd746c941bc8ee7e30231acece322338
akbarov099 <akbarov099@icloud.com>
2025-08-28 08:57:47 +0600

    ok

---
+++ 4-dars/main.js
@@ -0,0 +1,22 @@
+// let a = 1;
+
+// let b = 9;
+
+// alert(true || false)
+// alert(true && false)
+
+// alert(false || true)
+// alert(false && true)
+
+// if (a && b < 9) {
+//   alert("togri");
+// } else if (a || b > 10) {
+//   alert("pochti togri");
+// } else {
+//   alert("notogri");
+// }
+
+let b = prompt("raqam kirgizing");
+
+let proverka =
+  b < 5 ? alert("Salomat") : b > 5 ? alert("Hush kelibsiz") : alert("Uzur");
