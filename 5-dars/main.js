da5377186346ffc3572ebfcd4346eddbe73d7011
akbarov099 <akbarov099@icloud.com>
2025-08-30 14:17:44 +0600

    ok

---
+++ 5-dars/main.js
@@ -0,0 +1,74 @@
+// alert('Salom')
+// prompt('Ismingiz nima?')
+// confirm('Yoshingiz nechida?')
+
+// console.log('Salom Dunyo');
+
+// if (5 > 10) {
+//   console.log("Salom Dunyo");
+// } else if (5 !== 5) {
+//   console.log("Hello World");
+// } else {
+//   console.log("Xayr Dunyo");
+// }
+
+// let bootstrap = true ? console.log("Salom Dunyo")  : false  ? console.log("Hello World")  : console.log("Xayr Dunyo");
+
+// let user0 = 'Umar';
+// let user1 = 'Akbar';
+// let user2 = null;
+
+// console.log(user0 ?? user1 ?? user2 ?? "Foydalanuvchi nomi kiritilmagan");
+
+// let userAge = null;
+
+// userAge ??= 18;
+
+// console.log(userAge);
+
+
+// alert(undefined ?? " " ?? null ?? "" ?? NaN);
+
+
+// let city = null;
+
+// city ??= "Берлин";
+// city ??= 'Мюнхен';
+// city ??= "Кёльн";
+// city ??= "Гамбург";
+
+// alert(city);
+
+
+// let num1 = 10,
+//     num2 = 20,
+//     result;
+
+// if (result === null || result === undefined) {
+//   if (num1 !== null && num1 !== undefined) {
+//     result = num1;
+//   } else {
+//     result = num2;
+//   }
+// }
+
+// console.log(result);
+
+
+// let num1 = 10, num2 = 20, result;
+
+// result ??= num1 ?? num2;
+
+// console.log(result);
+
+// let city = null;
+// let defaultCity = "Tashkent";
+// let userCity;
+
+// if (city === null || city === undefined) {
+//   userCity = defaultCity;
+// } else {
+//   userCity = city;
+// }
+
+// console.log(userCity);
