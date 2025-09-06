c14c72a33a002a8656e98fa0bc5ce3c67d6b0b43
akbarov099 <akbarov099@icloud.com>
2025-09-06 14:39:52 +0600

    8-dars

---
+++ 8-dars/main.js
@@ -0,0 +1,71 @@
+// Hello();
+
+// function Hello() {
+//   alert("Hello World");
+// }
+
+// Hello();
+// Hello();
+
+// Hi();
+
+// let Hi = function() {
+//     alert("Hi World");
+// }
+
+// Hi();
+
+// Salom();
+
+// const Salom = () => {
+//     alert("Salom Dunyo");
+// }
+
+// Salom();
+
+// let lastname = "Aliyev";
+
+// function Name() {
+//   let lastname = "Valiyev";
+
+//   alert(lastname);
+// }
+
+// alert(lastname);
+
+// Name();
+
+// alert(lastname);
+
+// function Sum(a, b) {
+//   alert(a + b)
+// }
+
+// Sum(5, 10);
+
+// function name(firstname, lastname) {
+//   alert(firstname + " " + lastname);
+
+//   if (lastname === undefined) {
+//     alert("Familya kiritilmadi");
+//   }
+// }
+
+// name("Akbar", "Akbarov");
+
+// let age = prompt("Yoshingiz nechida?");
+
+// function chakeAge(age) {
+//   if (age >= 18) {
+//     return true;
+//   } else {
+//     return confirm("Sizga ota-onangizning roziligi bormi?");
+//     // alert(age);
+//   }
+// }
+
+// if (chakeAge(age)) {
+//   alert("Xush kelibsiz");
+// } else {
+//   alert("Kirish mumkin emas");
+// }
